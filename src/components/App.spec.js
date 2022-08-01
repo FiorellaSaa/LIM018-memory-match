6914d085a1d6fabec0c50b2962afbde3539b56a3
FiorellaSaa <fsaavedras.s@hotmail.com>
2022-08-01 10:34:57 -0500

    responsive

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -67,16 +67,4 @@ describe('shuffle', () => {
       expect(webdev.items).toHaveProperty('image');
     });
   });*/
-  /*describe('dataImagenes', () =>{
-  const webdev = [
-  {"bgColor": "#F7DF1E", "id": "js", "image": "https://simpleicons.org/icons/javascript.svg"}, 
-  {"bgColor": "#F05032", "id": "git", "image": "https://simpleicons.org/icons/git.svg"}, 
-  {"bgColor": "#1572B6", "id": "css", "image": "https://simpleicons.org/icons/css3.svg"}, 
-  {"bgColor": "#E34F26", "id": "html", "image": "https://simpleicons.org/icons/html5.svg"}, 
-  {"bgColor": "#339933", "id": "node", "image": "https://simpleicons.org/icons/nodedotjs.svg"}, 
-  {"bgColor": "#CB3837", "id": "npm", "image": "https://simpleicons.org/icons/npm.svg"}, 
-  {"bgColor": "#2C8EBB", "id": "yarn", "image": "https://simpleicons.org/icons/yarn.svg"}, 
-  {"bgColor": "#61DAFB", "id": "react", "image": "https://simpleicons.org/icons/react.svg"}, 
-  {"bgColor": "#DD0031", "id": "angular", "image": "https://simpleicons.org/icons/angular.svg"}, 
-  {"bgColor": "#4FC08D", "id": "vue", "image": "https://simpleicons.org/icons/vuedotjs.svg"},
-];*/
+
